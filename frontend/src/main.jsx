f3017bdf824319f4dfcc3595835ef3ad29c048af
Ram Gopal Gupta <RAMGOPAL42057@GMAIL.COM>
2026-09-24 10:28:43 +0530

    wire authentication and theme providers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles/global.css";
+import { AuthProvider } from "./auth/AuthContext.jsx";
+import { ThemeProvider } from "./theme/ThemeContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
